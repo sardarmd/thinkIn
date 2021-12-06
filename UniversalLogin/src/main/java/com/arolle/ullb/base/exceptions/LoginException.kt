fb06d6a82e29a0fd68b910befc6881d5435433d5
Mohammad <mohammad.sardar.ext@zeiss.com>
2021-12-06 14:09:54 +0530

    copyright text

--- UniversalLogin/src/main/java/com/arolle/ullb/base/exceptions/LoginException.kt
+++ UniversalLogin/src/main/java/com/arolle/ullb/base/exceptions/LoginException.kt
@@ -1,8 +1,12 @@
 package com.arolle.ullb.base.exceptions
 
-class LoginException( message:String ="LoginException",exceptionType:ExceptionTypes) : IllegalArgumentException(message) {
-
-}
+/**
+ * Copyright (c) 2021 Arolle solutions All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ * This is main class which will be exposed to clients
+ */
+class LoginException( message:String ="LoginException",exceptionType:ExceptionTypes) : IllegalArgumentException(message)
 
 enum class ExceptionTypes{
     INVALID_PHONE_NUMBER, INVALID_SOCIAL_ID, INVALID_APPLICATION, FACEBOOK_EXCEPTION,TWITTER_EXCEPTION,
--- UniversalLogin/src/main/java/com/arolle/ullb/sociallogin/listeners/callbacks.kt
+++ UniversalLogin/src/main/java/com/arolle/ullb/sociallogin/listeners/callbacks.kt
@@ -1,5 +1,10 @@
 package com.arolle.ullb.sociallogin.listeners
-
+/**
+ * Copyright (c) 2021 Arolle solutions All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ * This is main class which will be exposed to clients
+ */
 internal interface FacebookListener {
     fun onFacebookLoginSuccess()
     fun onFacebookLoginFail()
