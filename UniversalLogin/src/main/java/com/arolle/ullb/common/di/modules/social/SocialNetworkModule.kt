6f7b42246856625f8df0b7acab8caef2c35bd052
Mohammad <mohammad.sardar.ext@zeiss.com>
2021-12-18 16:38:46 +0530

    code cleaning

--- UniversalLogin/src/main/java/com/arolle/ullb/common/di/modules/social/SocialNetworkModule.kt
+++ UniversalLogin/src/main/java/com/arolle/ullb/common/di/modules/social/SocialNetworkModule.kt
@@ -6,34 +6,30 @@ package com.arolle.ullb.common.di.modules.social
  * license that can be found in the LICENSE file.
  */
 import com.arolle.ullb.common.di.scopes.GlobalScope
-import com.arolle.ullb.common.listeners.*
+import com.arolle.ullb.common.listeners.SocialNetworkLoginListener
 import com.arolle.ullb.sociallogin.fb.FacebookImpl
 import com.arolle.ullb.sociallogin.googleplus.GooglePlusImpl
 import com.arolle.ullb.sociallogin.instagram.InstagramImpl
 import com.arolle.ullb.sociallogin.twitter.TwitterImpl
 import dagger.Module
 import dagger.Provides
-import javax.inject.Named
 
 @Module
 object SocialNetworkModule {
 
     @Provides
-//    @Named("FBHelper")
+    @GlobalScope
     fun provideFacebookHelper(uiComponent: Any, listener: SocialNetworkLoginListener): FacebookImpl = FacebookImpl(uiComponent, listener)
 
     @Provides
     @GlobalScope
-//    @Named("TwitterHelper")
     fun provideTwitterHelper(listener: SocialNetworkLoginListener): TwitterImpl = TwitterImpl(listener)
 
     @Provides
     @GlobalScope
-//    @Named("GoogleHelper")
     fun provideGoogle(listener: SocialNetworkLoginListener): GooglePlusImpl = GooglePlusImpl(listener)
 
     @Provides
     @GlobalScope
-//    @Named("InstagramHelper")
     fun provideInstagramHelper(listener: SocialNetworkLoginListener): InstagramImpl = InstagramImpl(listener)
 }
